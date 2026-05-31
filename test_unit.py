f1290892f395f7a0f8946b94a6b6aa2667e2dc3a
Marfoq <youssef.marfoq@gmail.com>
2026-05-31 18:15:55 +0200

    Code change_2

--- test_unit.py
+++ test_unit.py
@@ -1,7 +1,7 @@
 import pytest
 from Test_Python_1 import  fonction
 
-Failures = {"ENGINE": 3, "BRAKE": 0, "TRANSMISSION": 2, "ABS": 5}
+Failures = {"ENGINE": 3, "BRAKE": 7, "TRANSMISSION": 2, "ABS": 5}
 
 def test_fonction():
-    assert fonction(Failures) == False
+    assert fonction(Failures) == True
